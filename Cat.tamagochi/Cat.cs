7491d2022738ffee6fb82f52cad1a4702c3cb502
funti <funti@VGOLIKO>
2023-03-09 01:35:19 +0700

    Добавьте файлы проекта.

---
+++ Cat.tamagochi/Cat.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+class Cat
+{
+    private int hunger;
+    private int health;
+
+    public Cat()
+    {
+        hunger = 0;
+        health = 10;
+    }
+
+    public void Eat(Fruit fruit)
+    {
+        health += fruit.GetHealth();
+        Console.WriteLine("Кошка съела " + fruit.GetName() + " и добавила " + fruit.GetHealth() + " здоровья.");
+    }
+
+    public void Meow()
+    {
+        Console.WriteLine("Мяу!");
+    }
+
+    public void Play()
+    {
+        health -= 2;
+        Console.WriteLine("Кошка играет.");
+    }
+
+    public void Run()
+    {
+        health -= 3;
+        Console.WriteLine("Кошка бегает.");
+    }
+
+    public void Update()
+    {
+        hunger++;
+        if (hunger < 5)
+        {
+            Meow();
+        }
+        else if (hunger >= 5 && hunger < 10)
+        {
+            Play();
+        }
+        else if (hunger >= 10)
+        {
+            Console.WriteLine("Кошка умерла от голода.");
+            Environment.Exit(0);
+        }
+    }
+}
+
+class Fruit
+{
+    private string name;
+    private int health;
+
+    public Fruit(string name, int health)
+    {
+        this.name = name;
+        this.health = health;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+}
---
+++ Cat.tamagochi/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Cat cat = new Cat();
+
+        while (true)
+        {
+            Console.WriteLine("Введите название фрукта (яблоко или апельсин):");
+            string fruitName = Console.ReadLine();
+            Fruit fruit = null;
+            if (fruitName == "яблоко")
+            {
+                fruit = new Fruit("Яблоко", 2);
+            }
+            else if (fruitName == "апельсин")
+            {
+                fruit = new Fruit("Апельсин", 3);
+            }
+            else
+            {
+                Console.WriteLine("Фрукт не распознан. Попробуйте еще раз.");
+                continue;
+            }
+
+            cat.Eat(fruit);
+            cat.Update();
+        }
+    }
+}
